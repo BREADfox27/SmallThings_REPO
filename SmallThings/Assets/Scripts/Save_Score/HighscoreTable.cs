d1d6476dee4993d4aecc39c616206e6abf27f96e
BREADfox27 <javierluna271107@gmail.com>
2026-08-20 01:35:44 +0200

    Update HighscoreTable.cs

--- SmallThings/Assets/Scripts/Save_Score/HighscoreTable.cs
+++ SmallThings/Assets/Scripts/Save_Score/HighscoreTable.cs
@@ -37,14 +37,6 @@ public class HighscoreTable : MonoBehaviour
             }
         }
 
-        if (highscores.highscoreEntryList.Count < 10)
-        {
-            for (int h = highscores.highscoreEntryList.Count; h > 10; h--)
-            {
-                highscores.highscoreEntryList.RemoveAt(10);
-            }
-        }
-
         highscoreEntryTransformList = new List<Transform>();
         foreach (HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
         {
@@ -54,74 +46,77 @@ public class HighscoreTable : MonoBehaviour
 
     private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
     {
-        float templateHeight = 60f;
-        Transform entryTransform = Instantiate(entryTemplate, container);
-        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
-        entryTransform.gameObject.SetActive(true);
-
-        int rank = transformList.Count + 1;
-        string rankString;
-        switch (rank)
+        if (transformList.Count <= 9)
         {
-            default:
-                rankString = rank + "TH";
-                break;
+            float templateHeight = 60f;
+            Transform entryTransform = Instantiate(entryTemplate, container);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+            entryTransform.gameObject.SetActive(true);
 
-            case 1:
-                rankString = "1ST";
-                break;
+            int rank = transformList.Count + 1;
+            string rankString;
+            switch (rank)
+            {
+                default:
+                    rankString = rank + "TH";
+                    break;
 
-            case 2:
-                rankString = "2ND";
-                break;
+                case 1:
+                    rankString = "1ST";
+                    break;
 
-            case 3:
-                rankString = "3RD";
-                break;
+                case 2:
+                    rankString = "2ND";
+                    break;
+
+                case 3:
+                    rankString = "3RD";
+                    break;
+            }
+
+            entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().text = rankString;
+
+            int score = highscoreEntry.score;
+            entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+            string name = highscoreEntry.name;
+            entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().text = name;
+
+            entryTransform.Find("Background").gameObject.SetActive(rank % 2 == 1);
+
+            if (rank == 1)
+            {
+                entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
+                entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
+                entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
+            }
+
+            ColorUtility.TryParseHtmlString("#FFD200", out Color color1);
+            ColorUtility.TryParseHtmlString("#C6C6C6", out Color color2);
+            ColorUtility.TryParseHtmlString("#B76F56", out Color color3);
+
+            switch (rank)
+            {
+                default:
+                    entryTransform.Find("Trophy").gameObject.SetActive(false);
+                    break;
+
+                case 1:
+                    entryTransform.Find("Trophy").GetComponent<Image>().color = color1;
+                    break;
+
+                case 2:
+                    entryTransform.Find("Trophy").GetComponent<Image>().color = color2;
+                    break;
+
+                case 3:
+                    entryTransform.Find("Trophy").GetComponent<Image>().color = color3;
+                    break;
+            }
+
+            transformList.Add(entryTransform);
         }
-
-        entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().text = rankString;
-
-        int score = highscoreEntry.score;
-        entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-        string name = highscoreEntry.name;
-        entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().text = name;
-
-        entryTransform.Find("Background").gameObject.SetActive(rank % 2 == 1);
-
-        if (rank == 1)
-        {
-            entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
-            entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
-            entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().color = Color.cyan;
-        }
-
-        ColorUtility.TryParseHtmlString("#FFD200", out Color color1);
-        ColorUtility.TryParseHtmlString("#C6C6C6", out Color color2);
-        ColorUtility.TryParseHtmlString("#B76F56", out Color color3);
-
-        switch (rank)
-        {
-            default:
-                entryTransform.Find("Trophy").gameObject.SetActive(false);
-                break;
-
-            case 1:
-                entryTransform.Find("Trophy").GetComponent<Image>().color = color1;
-                break;
-
-            case 2:
-                entryTransform.Find("Trophy").GetComponent<Image>().color = color2;
-                break;
-
-            case 3:
-                entryTransform.Find("Trophy").GetComponent<Image>().color = color3;
-                break;
-        }
-
-        transformList.Add(entryTransform);
     }
 
     public void AddHighscoreEntry(int newScore, string newName)
@@ -134,13 +129,6 @@ public class HighscoreTable : MonoBehaviour
         highscores.highscoreEntryList.Add(highscoreEntry);
 
         string json = JsonUtility.ToJson(highscores);
-        if (highscores.highscoreEntryList.Count < 10)
-        {
-            for (int h = highscores.highscoreEntryList.Count; h > 10; h--)
-            {
-                highscores.highscoreEntryList.RemoveAt(10);
-            }
-        }
         PlayerPrefs.SetString("highscoreTable", json);
         PlayerPrefs.Save();
     }
