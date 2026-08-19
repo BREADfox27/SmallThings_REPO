476a056dd7290f1e5a02efe1a32395894dc54ea1
BREADfox27 <javierluna271107@gmail.com>
2026-08-19 20:15:37 +0200

    Update HighscoreTable.cs

--- SmallThings/Assets/Scripts/Save_Score/HighscoreTable.cs
+++ SmallThings/Assets/Scripts/Save_Score/HighscoreTable.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using TMPro;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class HighscoreTable : MonoBehaviour
 {
     private Transform entryContrainer;
     private Transform entryTemplate;
-    
+
+    private List<HighscoreEntry> HighscoreEntryList;
+    private List<Transform> HighscoreEntryTransformList;
+
+
     private void Awake()
     {
         entryContrainer = transform.Find("HighscoreEntryContainer");
@@ -13,31 +19,57 @@ public class HighscoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 60f;
-        for (int i = 0; i < 10; i++)
+        HighscoreEntryList = new List<HighscoreEntry>()
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContrainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
+            new HighscoreEntry{ score = 10000, name = "AAA" },
+            new HighscoreEntry{ score = 21435, name = "PAS" },
+            new HighscoreEntry{ score = 567, name = "DIN" },
+            new HighscoreEntry{ score = 12677, name = "LOP" },
+            new HighscoreEntry{ score = 1, name = "KIK" },
+            new HighscoreEntry{ score = 96895, name = "BUT" },
+            new HighscoreEntry{ score = 2137724, name = "QER" },
+            new HighscoreEntry{ score = 1300, name = "MMM" },
+        };
 
-            int rank = i + 1;
-            string rankString;
-            switch (rank)
-            {
-                default: rankString = rank + "TH"; break;
-                case 1: rankString = "1ST"; break;
-                case 2: rankString = "2ND"; break;
-                case 3: rankString = "3RD"; break;
-            }
-
-            entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().text = rankString;
-
-            int score = Random.Range(0, 10000);
-            entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-            string name = "AAA";
-            entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().text = name;
+        HighscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in HighscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContrainer, HighscoreEntryTransformList);
         }
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 60f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+        switch (rank)
+        {
+            default: rankString = rank + "TH"; break;
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
+        }
+
+        entryTransform.Find("PosText").GetComponent<TextMeshProUGUI>().text = rankString;
+
+        int score = highscoreEntry.score;
+        entryTransform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
+
+        string name = highscoreEntry.name;
+        entryTransform.Find("NameText").GetComponent<TextMeshProUGUI>().text = name;
+
+        transformList.Add(entryTransform);
+    }
+
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
     }
 }
