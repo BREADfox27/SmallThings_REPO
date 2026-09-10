d7720287efe74afb301ce233c5bcc644d96c7ac4
BREADfox27 <javierluna271107@gmail.com>
2026-09-11 01:48:25 +0200

    Anim Trigger Test

--- SmallThings/Assets/Scripts/Menu_Anim/AnimTrigger.cs
+++ SmallThings/Assets/Scripts/Menu_Anim/AnimTrigger.cs
@@ -1,14 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class AnimTrigger : MonoBehaviour
 {
+    [SerializeField] float animDuration = 1f;
+    public Animator animator;
+    
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         
+    }
+
+    public void Btn()
+    {
+        StartCoroutine(BtnAnim());
+    }
+
+    public IEnumerator BtnAnim()
+    {
+        animator.SetTrigger("StartAnim");
+        yield return new WaitForSeconds(animDuration);
+        animator.SetTrigger("EndAnim");
     }
 }
